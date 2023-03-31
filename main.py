df45ec0a27809ee1ba8410caf68c4c2000696cfe
korsem <korneliasem@gmail.com>
2023-03-31 12:29:10 +0200

    basic snake

--- main.py
+++ main.py
@@ -15,21 +15,33 @@ class FRUIT:
     def draw_fruit(self):
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, (105, 0, 60), fruit_rect)
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
 
 class SNAKE:
     def __init__(self):
         # snake is stored in a list
-        self.body = [Vector2(5, 7), Vector2(6, 7), Vector2(7,7)]
+        self.body = [Vector2(4, 7), Vector2(3, 7), Vector2(2,7)]
         self.direction = Vector2(1, 0) # default moves to the left
-
+        self.new_block = False
     def draw_snake(self):
         for elements in self.body:
             snake_rect = pygame.Rect(int(elements.x * cell_size), int(elements.y * cell_size), cell_size, cell_size)
             pygame.draw.rect(screen, (170, 00, 60), snake_rect)
     def move_snake(self):
-        body_copy = self.body[:-1] # without the last element
+        if self.new_block == False:
+            body_copy = self.body[:-1] # without the last element
+        else:
+            body_copy = self.body[:] # with the last element because snake has eaten the snack
+            self.new_block = False
         body_copy.insert(0, body_copy[0] + self.direction) # snake is moving by the direction
         self.body = body_copy[:]
+        screen.fill((180, 215, 70)) # I need to think of a better way to update the screen (without that you can still see the "old" parts of the snake)
+
+    def add_block(self):
+        self.new_block = True
 
 class MAIN:
     def __init__(self):
@@ -37,16 +49,36 @@ class MAIN:
         self.fruit = FRUIT()
     def update(self):
         self.snake.move_snake()
-
+        self.check_collision()
+        self.check_fail()
     def draw_elements(self):
         self.fruit.draw_fruit()
         self.snake.draw_snake()
 
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit.randomize()
+            self.snake.add_block()
+
+    def game_over(self):
+        pygame.quit()
+        exit()
+        print("ups")
+
+    def check_fail(self):
+        if not (0 <= self.snake.body[0].x < cell_number and 0 <= self.snake.body[0].y < cell_number): # if snake's head hits the walls its game over
+            self.game_over()
+        for element in self.snake.body[1:]: # if snake hits itself its game over
+            if element == self.snake.body[0]:
+                self.game_over()
+
+
+
 # setting up the window
 pygame.init()
 
-cell_size = 20
-cell_number = 10
+cell_size = 35
+cell_number = 15
 
 screen = pygame.display.set_mode((cell_size * cell_number, cell_number*cell_size))
 pygame.display.set_caption("Snake")
@@ -76,13 +108,13 @@ while True:
         if event.type == SCREEN_UPDATE:
             main_game.update()
         if event.type == pygame.KEYDOWN: # keyboard controll
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and main_game.snake.direction.y != 1:
                 main_game.snake.direction = Vector2(0, -1)
-            elif event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN and main_game.snake.direction.y != -1:
                 main_game.snake.direction = Vector2(0, 1)
-            elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT and main_game.snake.direction.x != 1:
                 main_game.snake.direction = Vector2(-1, 0)
-            elif event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT and main_game.snake.direction.x != -1:
                 main_game.snake.direction = Vector2(1, 0)
     main_game.draw_elements()
     pygame.display.update()
